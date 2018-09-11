41cef6dec0e1cf59abca70352d1869b1b27886de
Karl Messner <karlmessner@gmail.com>
2018-09-11 09:25:57 -0400

    nothing

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?PHP echo "<a href=s3.php>upload</a>"; ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <img src="https://s3.amazonaws.com/mc-vid-submissions/DSC02196.JPG" />
+  </body>
+</html>
