c16da8d475bfe5524607f52cc482f1be3d5303ef
Karl Messner <karlmessner@gmail.com>
2018-09-20 16:04:23 -0400

    removing executables locations

--- videos/index.php
+++ videos/index.php
@@ -19,7 +19,6 @@ foreach ($files1 as $file) {
 	
 function fixVideo($file) {
 $config = array(
-
 'timeout' => 3600, // The timeout for the underlying process
 'ffmpeg.threads' => 12, // The number of threads that FFMpeg should use
 );
