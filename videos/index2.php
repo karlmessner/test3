481adb3cecf4cd055287848cce9fcfb7ce686be6
Karl Messner <karlmessner@gmail.com>
2018-09-20 23:52:51 -0400

    index2

--- videos/index2.php
+++ videos/index2.php
@@ -5,8 +5,6 @@ ini_set("display_errors", 1);
 
 $file="appL.mp4";
 $config = array(
-'ffmpeg.binaries' => '/usr/local/bin/ffmpeg',
-'ffprobe.binaries' => '/usr/local/bin/ffprobe',
 'timeout' => 3600, // The timeout for the underlying process
 'ffmpeg.threads' => 12, // The number of threads that FFMpeg should use
 );
