9543e9147b6d2d959fe8191a3f262f5ae76fe1fd
Karl Messner <karlmessner@gmail.com>
2018-09-28 13:08:10 -0400

    Marc help

--- includes/con.php
+++ includes/con.php
@@ -1,9 +1,11 @@
 <?PHP 
 session_start();
+
 /*
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 */
+
 
 
 // SET CURRENT TIMEZONE
@@ -20,8 +22,20 @@ $DB_SERVER 		= $_ENV['MYSQL_SUBMISSIONS_DBSERVER'];
 $DB_USERNAME 	= $_ENV['MYSQL_SUBMISSIONS_USER'];
 $DB_PASSWORD 	= $_ENV['MYSQL_SUBMISSIONS_PASSWORD'];
 $DB_DATABASE 	= $_ENV['MYSQL_SUBMISSIONS_DB'];
+$DB_URL 		= $_ENV['DATABASE_URL'];
 
+/*
 $db = mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD) or die(mysqli_error($db));
+mysqli_ssl_set($db,NULL,NULL,NULL,'config',NULL);
+mysqli_real_connect($db);
+*/
+
+$db = mysqli_init();
+mysqli_ssl_set($db,NULL,NULL,'config/rds-combined-ca-bundle.pem',NULL,NULL);
+mysqli_real_connect($db,$DB_SERVER, $DB_USERNAME, $DB_PASSWORD);
+
+
+
 $database = mysqli_select_db($db, $DB_DATABASE) or die(mysqli_error($db));
 
 $now = time();
