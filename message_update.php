dbca3aadb937e2635888d68c5d116546cf783f70
Karl Messner <karlmessner@gmail.com>
2018-10-01 22:51:15 -0400

    final update to message

--- message_update.php
+++ message_update.php
@@ -22,7 +22,12 @@ if ($_REQUEST['pk']!=$goodKey){
     <meta charset="utf-8">
     <title>MESSAGES-moodcaster</title>
     
-    <style>*{font-family: sans-serif}</style>
+    <style>
+	    *{font-family: sans-serif}
+	    input[type=text]{font-size: 16px;width:400px;text-align: center;}
+	    form  {text-align:center;}
+	    
+	</style>
   </head>
   <body>
 <br />
@@ -32,11 +37,12 @@ if ($_REQUEST['pk']!=$goodKey){
 <br /><br />
 <?PHP 
 	if ($_POST['newMessage']){
-		$sql = "INSERT INTO mc_downstream_messages SET mdm_message='".$_POST['mdm_message']."',mdm_current_version='".$_POST['mdm_current_version']."', mdm_must_update='".$_POST['mdm_must_update']."'";
+		$sql = "INSERT INTO mc_downstream_messages SET mdm_date = '$now', mdm_message='".$_POST['mdm_message']."',mdm_current_version='".$_POST['mdm_current_version']."'";
 		mysqli_query($db, $sql); echo mysqli_error($db);
 		//echo $sql;
 			}
 	
+	// UNCOMMENTING THIS LINE DELETES THE ENTIRE DATABASE
 	//$sql = "delete from mc_downstream_messages where mdm_id>0";		mysqli_query($db, $sql); echo mysqli_error($db);
 	
 ?>
@@ -44,16 +50,14 @@ if ($_REQUEST['pk']!=$goodKey){
 <form action="#" method="post">
 	<input type="hidden" name="pk" value="<?PHP echo $_ENV['GOODKEY'];  ?>" />
 	<input type='text' name='mdm_message' placeholder = "message" /><br /><br />
-	<input type='text' name='mdm_current_version' placeholder = "Current Version" /><br /><br />
-	<input type=checkbox name="mdm_must_update" id='must' value='1'/>&nbsp;<label for="must">MUST UPDATE</label><br/><br />
+	<input type='text' name='mdm_current_version' placeholder = "Force Update if version less than" /><br /><br />
 	<input type='submit' name='newMessage' value='ADD MESSAGE'/><br />
 </form>
 </br><?PHP
 
 
-
 // LIST ALL MESSAGES
-$sql = "SELECT mdm_id, mdm_date ,  mdm_message,mdm_must_update,mdm_current_version from mc_downstream_messages order by mdm_date desc";
+$sql = "SELECT mdm_id, mdm_date ,  mdm_message,mdm_current_version from mc_downstream_messages order by mdm_date desc";
 $rsMESSAGE = mysqli_query($db,$sql);
 echo mysqli_error($db);
 $numMESSAGES = mysqli_num_rows($rsMESSAGE); 
@@ -61,11 +65,11 @@ if ($numMESSAGES>0){
 
 echo "<h2>PAST MESSAGES</h2>";	
 echo "<table cellspacing=10 cellpadding=10>";	
-		echo "<tr><td><strong>ID</strong></td><td><strong>DATE</strong></td><td><strong>MUST UPDATE</strong></td><td><strong>CURRENT VERSION</strong></td><td><strong>MESSAGE</strong></td></tr>";
+		echo "<tr><td><strong>ID</strong></td><td><strong>DATE</strong></td><td><strong>UPDATE BELOW VERSION</strong></td><td><strong>MESSAGE</strong></td></tr>";
 
 	while ($thisMessage = mysqli_fetch_array($rsMESSAGE)){
 		extract($thisMessage);
-		echo "<tr><td>$mdm_id</td><td>" . date("m.d.y g:ia",$mdm_date) . "</td><td>$mdm_must_update</td><td>$mdm_current_version</td><td>$mdm_message</td></tr>";
+		echo "<tr><td>$mdm_id</td><td>" . date("m.d.y g:ia",$mdm_date) . "</td><td>$mdm_current_version</td><td>$mdm_message</td></tr>";
 		
 	}
 echo "</table>";	
