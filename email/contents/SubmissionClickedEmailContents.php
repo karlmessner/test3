43a20badfdef30ec821908e8eaea0472d43ca57c
Karl Messner <karlmessner@gmail.com>
2018-10-08 09:58:58 -0400

    send clicked email notification

---
+++ email/contents/SubmissionClickedEmailContents.php
@@ -0,0 +1,15 @@
+<img src="https://www.moodcaster.com/font-to-image.php?s=28&t=They're Watching! " />
+
+<br />
+
+<p>Hey, {{Name}},</p>
+
+<p>{{shortRecipEmail}}  just clicked play. Pass the popcorn—this is gonna be good! 🤩 </p>
+
+<p>Moodcaster Team</p>
+
+<p class="subhead">Reading for <strong>{{Role}}</strong><br/>in <strong>{{Title}}</strong></p>
+<br />
+
+<p class='bottomspace'>&nbsp;</p> 
+
