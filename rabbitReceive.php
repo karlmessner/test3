6647d694e0412655f288bcfc7c6d908dda0c17f2
Karl Messner <karlmessner@gmail.com>
2018-10-10 15:06:31 -0400

    trying to fix crashing worker

--- rabbitReceive.php
+++ rabbitReceive.php
@@ -26,25 +26,22 @@ $ch->queue_bind($queue, $exchange);
 
 echo "step 3<BR>";
 
-
-$callback = function ($msg) {
-    $comment =  ' [x] Received '. $msg->body . "\n";
-	echo "COMMENT: $comment <BR>";
-    $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
-};
+$retrived_msg = $ch->basic_get($queue);
+echo sprintf("Message recieved: %s\n", $retrived_msg->body);
+$ch->basic_ack($retrived_msg->delivery_info['delivery_tag']);
 
 echo "step 4<BR>";
 
-// $ch->basic_qos(null, 1, null);
-$ch->basic_consume($queue, '', false, false, false, false, $callback);
+
+
+while (count($ch->callbacks)) {
+    $ch->wait();
+}
 
 echo "step 5<BR>";
 
-/*
-while (count(ch->callbacks)) {
-    $ch->wait();
-}
-*/
+
+
 $ch->close();
 $conn->close();
 echo "step 6<BR>";
