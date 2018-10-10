f422178420f160f89cb57a6fa75ad9f83188878a
Karl Messner <karlmessner@gmail.com>
2018-10-10 17:18:05 -0400

    trying to fix crashing worker

--- rabbitReceive.php
+++ rabbitReceive.php
@@ -2,16 +2,12 @@
 require './vendor/autoload.php';
 require 'env.php';
 include('includes/con.php');
-$debug = false;
 
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-if ($debug) {echo "trying<BR>";}
-
 $sql = "insert into TESTvideoQueue set content = 'trying'";
 mysqli_query($db, $sql);
-
 
 define('AMQP_DEBUG', true);
 use PhpAmqpLib\Connection\AMQPConnection;
@@ -20,8 +16,6 @@ $url = parse_url(getenv('CLOUDAMQP_URL'));
 $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
 $ch = $conn->channel();
 
-if ($debug) {echo "step 2<BR>";}
-
 $exchange = 'amq.direct';
 $queue = 'Video_Process_queue';
 $ch->queue_declare($queue, false, true, false, false);
@@ -29,37 +23,21 @@ $ch->exchange_declare($exchange, 'direct', true, true, false);
 $ch->queue_bind($queue, $exchange);
 
 
-if ($debug) {echo "step 3<BR>";}
+function doIt($ch,$queue,$db){
+	$retrived_msg = $ch->basic_get($queue);
+	$payload = $retrived_msg->body;
+	
+	$sql = "insert into TESTvideoQueue set content = '$payload'";
+	mysqli_query($db, $sql);
+	$ch->basic_ack($retrived_msg->delivery_info['delivery_tag']);
+	}
+	
+doIt($ch,$queue,$db);	
 
-$retrived_msg = $ch->basic_get($queue);
-$payload = $retrived_msg->body;
-
-if ($debug) {echo "Message recieved: $payload <BR>";}
-
-$sql = "insert into TESTvideoQueue set content = '$payload'";
-mysqli_query($db, $sql);
-
-
-if ($debug) {echo "step 4<BR>";}
-
-
-
-$ch->basic_ack($retrived_msg->delivery_info['delivery_tag']);
-
-if ($debug) {echo "step 5<BR>";}
-
-if ($debug) {echo "<pre>";var_dump($ch);echo "</pre><BR><BR>";}
-
-$i=1;
 while (count($ch->callbacks)) {
     $ch->wait();
-    if ($debug) {echo "$i <BR>"; $i++;}
 }
-
-echo "step 6<BR>";
-
 
 
 $ch->close();
 $conn->close();
-echo "step 7<BR>";
