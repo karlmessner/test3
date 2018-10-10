0186b1496c7e6f722761e1c68ab81f82b4800390
Karl Messner <karlmessner@gmail.com>
2018-10-10 02:20:06 -0400

    trying to fix crashing worker

--- rabbitReceive.php
+++ rabbitReceive.php
@@ -3,9 +3,14 @@ require './vendor/autoload.php';
 require 'env.php';
 include('includes/con.php');
 
+/*
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+*/
 
 
-define('AMQP_DEBUG', false);
+
+define('AMQP_DEBUG', true);
 use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 $url = parse_url(getenv('CLOUDAMQP_URL'));
@@ -19,49 +24,16 @@ $ch->exchange_declare($exchange, 'direct', true, true, false);
 $ch->queue_bind($queue, $exchange);
 
 
-
-
-
-
-/* new way? */
-$callback = function($msg) {
-    
-    try {
-
-		$payload = $msg->body;
-		$payload = print_r($retrived_msg,1);
-		
-		$sql = "insert into TESTvideoQueue set content = '$payload'";
-		mysqli_query($db, $sql);
-
-    } catch(Exception $e) {
-
-
-    }
-};
-$ch->basic_qos(null, 1, null);
-$ch->basic_consume($queue, '', false, false, false, false, $callback);
-/* .new way*/
-
-
-
-
-
-/* old way */
-/*
 $retrived_msg = $ch->basic_get($queue);
 //var_dump($retrived_msg->body);
 
 $payload = $retrived_msg->body;
-$payload = print_r($retrived_msg,1);
+
 
 $sql = "insert into TESTvideoQueue set content = '$payload'";
 mysqli_query($db, $sql);
 
 $ch->basic_ack($retrived_msg->delivery_info['delivery_tag']);
-*/
-/* .old way */
-
 
 
     while (count($ch->callbacks)) {
@@ -69,5 +41,7 @@ $ch->basic_ack($retrived_msg->delivery_info['delivery_tag']);
     }
     
     
+/*
 $ch->close();
 $conn->close();
+*/
