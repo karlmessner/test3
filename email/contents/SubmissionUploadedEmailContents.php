25f717b45d56d66550f50defa8d1373244e799f5
Karl Messner <karlmessner@gmail.com>
2018-10-11 11:37:49 -0400

    Added additional email

---
+++ email/contents/SubmissionUploadedEmailContents.php
@@ -0,0 +1,23 @@
+<img src="https://www.moodcaster.com/font-to-image.php?s=28&t= " />
+
+<br />
+
+<p>Hey, {{Name}},</p>
+
+<p>Your submission has been uploaded to Moodcaster. </p>
+
+
+<p style="font-size:24px;">🧙‍♂️🧝‍♀️</p>
+
+<p>Our elves are busily packaging it to send to your recipients. That might take a few minutes&emdash; they have tiny hands. When the elves are done, we'll send it to your recipients and send you a second email that it got off successfully.</p>
+
+
+<p>Team Moodcaster</p>
+
+<p class="subhead">Reading for <strong>{{Role}}</strong><br/>in <strong>{{Title}}</strong></p>
+<br />
+
+<p>P.S. This is the <a href="{{downloadLink}}" target="_blank">Link</a> They'll get and you can share with others. Once the elves are done, your video will play there. <strong>We won't send the email to your recipients until the video is done.</strong></p>
+
+<p class='bottomspace'>&nbsp;</p> 
+
