c64c2610afcbb175226fc5b82df87ba09feec8a5
Karl Messner <karlmessner@gmail.com>
2018-10-11 06:00:23 -0400

    download-not-ready

---
+++ download-not-ready.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+	
+	<link rel="stylesheet" href=media/css/download.css />
+	<link rel="stylesheet" href=media/css/download-right-default.css />
+	
+		
+	<meta http-equiv=\"refresh\" content=\"10;URL='<?PHP echo urldecode($_GET['d']);?>'" />"
+		
+		
+		
+		
+		
+	
+  </head>
+  <body>
+		<div id="wrapper">
+			<div id="masthead">
+				<div id="logo">
+					<a href="http://www.moodcaster.com/casting-professional.html" target='_blank'><img src="media/images/moodcaster-red.svg" /></a>
+				</div><!-- logo-->			
+				<div id="toplinks">
+				</div><!-- toplinks-->
+			</div><!-- masthead-->
+			
+			<div id="videoDiv">
+			<div id="vidbox">
+			<video id='video'	
+				autoplay="true"
+					loop="true"
+				src="media/video/Standby.mp4"
+				width="100%"
+			       >
+			    Sorry, your browser doesn't support embedded videos.
+			</video>				
+			</div><!-- vidbox-->
+			<div id="downloadSubmissiondiv">
+			</div><!-- downloadSubmissiondiv-->
+			
+			<div id="Sharedivwrapper" class=''>
+
+			<div id="Sharediv">
+			</div><!-- Sharediv-->
+			<div id="Sharedivhidden" >
+			</div><!-- Sharediv-->
+			
+			</div><!-- shared div wrapper -->
+			</div><!-- videoDiv-->			
+			
+			<div id="right">
+				
+			<div id="rightContainer">
+			<div id="logoBox">
+				<img src="media/images/logo-white-m-red-square.png" alt="logo-white-m-red-square"  />
+					
+					<span id="logotext">Casting Simplified</span>
+				
+			</div><!-- logoBox-->
+			<div id="titleRightBox">
+				<h1>Need a simpler way to review submissions?</h1>
+			</div><!-- titleRightBox-->
+			<div id="copyRightBox">
+				<p>Streamline your submissions into a single dashboard designed for collaboration. Share your casting breakdowns anywhere through a simple yet powerful technology we call moodcodes. Suddenly life is a lot easier.</p>
+			</div><!-- copyRightBox-->
+			<div id="ctaRightBox">
+				<a href="http://www.moodcaster.com/casting-professional.html" target='_blank'>Let&apos;s do this!</a>
+			</div><!-- ctaRightBox-->
+			</div><!-- rightContainer-->	
+				
+				
+				
+			</div><!-- right-->			
+			
+			
+						
+		</div><!-- wrapper -->
+	</body>
+</html>			
+			
+			
