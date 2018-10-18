14d729ad9142089fc2bcd8d5c24fb90dd5ba3186
Karl Messner <karlmessner@gmail.com>
2018-10-18 11:50:24 -0400

    ALP download

--- download_file.php
+++ download_file.php
@@ -29,8 +29,22 @@ if (!$n){
 $sql =  "UPDATE mc_submissions SET mc_download='$now' where mc_id='$s' LIMIT 1";
 mysqli_query($db,$sql);echo mysqli_error($db);
 }
-$location =  "Location: $mc_zip_file_url";
-header($location);
+
+// IF ALP SUBMISSION, SEND STITCHED FILE, OTHERWISE, GIVE ZIP FILE
+
+if ($mc_alp == 1){
+	$location =  "Location: $mc_stitch_file_url";
+	$file_name = $mc_name . " _Audtion_Taping_" . date($mc_creation, "(m-d-y).mp4");
+	header('Content-Type: application/octet-stream');
+	header("Content-Transfer-Encoding: Binary"); 
+	header("Content-disposition: attachment; filename=\"".$file_name."\""); 
+	readfile($mc_stitch_file_url);
+	exit;
+	}else{
+		$location =  "Location: $mc_zip_file_url";
+		header($location);
+	}
+	
 
 
 
