863bb61bdb57f548eac70da970880b5ac0f8923b
Karl Messner <karlmessner@gmail.com>
2018-10-18 10:18:49 -0400

    ADDING ALP VIDEO PROCESSING

---
+++ email/contents/ALPRecipientsEmailContents.php
@@ -0,0 +1,16 @@
+<table style="padding:0;margin:0"><tr style="padding:0;margin:0">
+<td class='lefthalf' style="padding:0;margin:0">
+	<img src="https://www.moodcaster.com/font-to-image.php?s={{firstLineSize}}&t={{firstLineText}}" />
+	<br />
+	<img src="https://www.moodcaster.com/font-to-image.php?s={{secondLineSize}}&t={{secondLineText}}" />
+	<p class="subhead">Reading for <strong>{{Role}}</strong><br/>in <strong>{{Title}}</strong></p>
+	<p align="center" class="downloadp"><a class="downloadButton" href="{{downloadLink}}" target="_blank">View Submission</a>
+	<br />
+	<a href="http://www.moodcaster.com/casting-professional.html" class="getMoodcasterLink" target="_blank">Get Moodcaster for casting pro's</a>
+	</p>
+	<p class='bottomspace'>&nbsp;</p>
+</td>
+<td class="imgtd">
+	<img src="{{Profile_shot}}" class="thumb" width="200" height="200"/>
+</td>
+</tr></table>
