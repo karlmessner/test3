25fd508ead68b30907724c486d56b15ae7510f27
Karl Messner <karlmessner@gmail.com>
2018-10-19 01:46:43 -0400

    alp download

---
+++ template-download-right-cp.php
@@ -0,0 +1,12 @@
+<div id="rightContainer">
+<div id="titleRightBox">
+	<h1 class='redscore'>Casting directors:</h1>
+	<h1>Need a simpler way to review submissions?</h1>
+</div><!-- titleRightBox-->
+<div id="copyRightBox">
+	<p>Streamline your submissions into a single dashboard designed for collaboration. Share your casting breakdowns anywhere through a simple yet powerful technology we call moodcodes. Suddenly life is a lot easier.</p>
+</div><!-- copyRightBox-->
+<div id="ctaRightBox">
+	<a href="http://www.moodcaster.com/casting-professional.html" target='_blank'>Join Now &rarr;</a>
+</div><!-- ctaRightBox-->
+</div><!-- rightContainer-->	
