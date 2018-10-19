185eec9b14ac5446b284dfb798cebd121beb73bc
Karl Messner <karlmessner@gmail.com>
2018-10-19 01:46:39 -0400

    alp download

---
+++ template-download-right-ALP.php
@@ -0,0 +1,19 @@
+<div id="rightContainer">
+<div id="titleRightBox">
+	<h1 class='redscore'>New iPhone audition app</h1>
+	<h1>Self-tape Heaven: moodcaster</h1>
+</div><!-- titleRightBox-->
+<div id="copyRightBox">
+	<p>Hi $Name,</p>
+<p>If you're ever in a pinch and need to self-tape an audition,
+The moodcaster app is perfect for that!</p>
+</div><!-- copyRightBox-->
+<div id="ctaRightBox">
+	<a href="http://www.moodcaster.com/talent.html" target='_blank' style="display:inline-block; margin-right: 10px">Tell me more! &rarr;</a>
+	<a href="https://itunes.apple.com/us/app/moodcaster/id1374556552?mt=8" target='_blank' style="display:inline-block;">Get moodcaster &rarr;</a>
+</div><!-- ctaRightBox-->
+</div><!-- rightContainer-->	
+
+
+
+
