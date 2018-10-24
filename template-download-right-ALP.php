eca1678574f2342952b55849e69f51af25d33171
Karl Messner <karlmessner@gmail.com>
2018-10-24 11:17:13 -0400

    ALP download page additions

--- template-download-right-ALP.php
+++ template-download-right-ALP.php
@@ -3,8 +3,7 @@
 	<h1>Hi $Name,</h1>
 </div><!-- titleRightBox-->
 <div id="copyRightBox">
-<p>If you're ever in a pinch and need to self-tape an audition,
-The moodcaster app is perfect for that!</p>
+<p>Caught in a pinch and need to self-tape an audition? Moodcaster has everything you need to self-tape, audition, and submit with confidence.</p>
 </div><!-- copyRightBox-->
 <div id="ctaRightBox">
 	<a href="http://www.moodcaster.com/talent.html" target='_blank' style="display:inline-block; margin-right: 10px">Tell me more! &rarr;</a>
