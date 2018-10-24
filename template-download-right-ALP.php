ac0ff9684a23721ed48980cffcae8348b69804c2
Karl Messner <karlmessner@gmail.com>
2018-10-24 17:26:18 -0400

    Guy’s comma

--- template-download-right-ALP.php
+++ template-download-right-ALP.php
@@ -1,6 +1,6 @@
 <div id="rightContainer" style="top:20px;">
 <div id="titleRightBox">
-	<h1>Hi, $Name,</h1>
+	<h1>Hi, $Name</h1>
 </div><!-- titleRightBox-->
 <div id="copyRightBox">
 <p>Caught in a pinch and need to self-tape an audition? Moodcaster has everything you need to self-tape, audition, and submit with confidence.</p>
